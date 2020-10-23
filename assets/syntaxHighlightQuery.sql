064ce0c7b50383959eeb36dbc52299091c80a479
tanyogeorgiev <tanyogeorgiev@yahoo.com>
2020-10-24 01:23:05 +0300

    Add some tabs ;)

--- assets/syntaxHighlightQuery.sql
+++ assets/syntaxHighlightQuery.sql
@@ -1,26 +1,27 @@
 
-    DECLARE @variableInt        INT,
-            @variableString     VARCHAR(30)
+        DECLARE @variableInt        INT,
+                @variableString     VARCHAR(30)
 
-    SET @variableInt = 23
-    SET @variableString = '100'
 
-    SELECT  TOP 1
-            CurrentDate = GETDATE(),
-            SomeText    = N'Some strings' + CHAR(13) + CHAR(10) + 'Another string....',
-            UserId      = CURRENT_USER,
-            UserSPID    = @@SPID,
-            SomeCalcs   = 2/2 + @variableInt + CONVERT(INT,LEFT(@variableString,2)),
-            SomeNulls   = ISNULL(NULL,'this is null')
+        SET     @variableInt    = 23
+        SET     @variableString = '100'
 
-    FROM    sys.all_columns AS c
-            LEFT OUTER JOIN sys.all_objects AS o
-            ON c.[object_id] = o.[object_id]
+        SELECT  TOP 1
+                CurrentDate = GETDATE(),
+                SomeText    = N'Some strings' + CHAR(13) + CHAR(10) + 'Another string....',
+                UserId      = CURRENT_USER,
+                UserSPID    = @@SPID,
+                SomeCalcs   = 2/2 + @variableInt + CONVERT(INT,LEFT(@variableString,2)),
+                SomeNulls   = ISNULL(NULL,'this is null')
 
-    WHERE   @variableInt IS NOT NULL
-    /* ORDER BY something */
-    ORDER BY c.name;
+        FROM    sys.all_columns AS c
+                LEFT OUTER JOIN sys.all_objects AS o
+                ON c.[object_id] = o.[object_id]
 
-    -- And another comment line 
+        WHERE   @variableInt IS NOT NULL
+        /* ORDER BY something */
+        ORDER BY c.name;
 
-    EXEC sys.sp_help;
+        -- And another comment line 
+
+        EXEC sys.sp_help;
